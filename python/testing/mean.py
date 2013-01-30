b1bdef8cb32091891a258f4b39ff4ee6cfc8130a
Katy Huff <katyhuff@gmail.com>
2013-01-30 08:56:03 -0600

    removes answers accidentally added. These were flotsam/jetsam from live coding in the bootcamp.

--- python/testing/mean.py
+++ python/testing/mean.py
@@ -4,8 +4,6 @@ def mean(numlist):
         length = len(numlist)
     except TypeError :
         raise TypeError("The list was not numbers.")
-    except ZeroDivisionError :
-        raise ZeroDivisionError("Does your list have elements in it?")
     except :
         print "Something unknown happened with the list."
-    return float(total)/float(length)
+    return total/length
