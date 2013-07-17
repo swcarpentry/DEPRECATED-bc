e3332e0ff66402d02826f7baa69c28e1631e0b87
Lynne Williams <williams.lynne99@gmail.com>
2013-07-17 16:41:39 -0700

    updated createdloftus.m and csv file

--- createloftus.m
+++ createloftus.m
@@ -1,30 +1,28 @@
 clear
 
-ncond=5;
-ncondobs=50000;
-nobs=ncond*ncondobs;
+ncond=5; % number of conditions
+ncondobs=50000; % number of observations in each condition (n)
+nobs=ncond*ncondobs; % total number of observations (N)
 
+
+%%%%%%%%
 % Create variables of n=50000 with the same mean and variance as the Loftus
 % example in Abdi et al. 2010.
 
-Contact=normrnd(30,10.7909427,50000);
-
-Hit=normrnd(35,9.297550454,50000);
-
-Bump=normrnd(38,11.05541597,50000);
-
-Collide=normrnd(41,6.446359869,50000);
-
-Smash=normrnd(46,5.773502692,50000);
+Contact=normrnd(30,10.7909427,ncondobs);
+Hit=normrnd(35,9.297550454,ncondobs);
+Bump=normrnd(38,11.05541597,ncondobs);
+Collide=normrnd(41,6.446359869,ncondobs);
+Smash=normrnd(46,5.773502692,ncondobs);
 
 % Now concatenate the data
 R=[Contact; Hit; Bump; Collide; Smash];
 
 % create a vector with the condition numbers (1=Contact; 2=Hit; 3=Bump;
 % 4=Collide; 5=Smash
-r=ones(50000,1);
+r=ones(ncondobs,1);
 Group=[];
-for a=1:5;
+for a=1:ncond;
     Group=[Group; r*a];
 end
 clear r
@@ -40,7 +38,7 @@ n=n+1; nomvar{n}='Smash';
 % Create vector of condition names for each observation
 m=0;
 for i= 1:n
-     for j=1:50000     
+     for j=1:ncondobs     
           m=m+1;
           nomobs{m}=nomvar{i};
      end
@@ -49,34 +47,60 @@ end
 
 
 % Now create a variable for reaction time
-RT=normrnd(300,140,250000);
+RT=normrnd(300,140,nobs);
 
 % Now create a variable for age (need some variability in the data.
-Age=nan(250000,1);
-Age(1:50000,1)=normrnd(42.25,16,50000);
-Age(50001:100000,1)=normrnd(42,16,50000);
-Age(100001:150000,1)=normrnd(41.75,17,50000);
-Age(150001:200000,1)=normrnd(41.5,15,50000);
-Age(200001:250000,1)=normrnd(41.25,18,50000);
+Age=nan(nobs,1);
+Age(1:50000,1)=normrnd(42.25,16,ncondobs);
+Age(50001:100000,1)=normrnd(42,16,ncondobs);
+Age(100001:150000,1)=normrnd(41.75,17,ncondobs);
+Age(150001:200000,1)=normrnd(41.5,15,ncondobs);
+Age(200001:250000,1)=normrnd(41.25,18,ncondobs);
 Age=round(Age);
 
 % Now create a variable for IQ
-IQ=nan(250000,1);
-IQ(1:50000,1)=normrnd(100.5,16,50000);
-IQ(50001:100000,1)=normrnd(100,15,50000);
-IQ(100001:150000,1)=normrnd(99.8,14,50000);
-IQ(150001:200000,1)=normrnd(99,16,50000);
-IQ(200001:250000,1)=normrnd(100.5,14,50000);
+IQ=nan(nobs,1);
+IQ(1:50000,1)=normrnd(100.5,16,ncondobs);
+IQ(50001:100000,1)=normrnd(100,15,ncondobs);
+IQ(100001:150000,1)=normrnd(99.8,14,ncondobs);
+IQ(150001:200000,1)=normrnd(99,16,ncondobs);
+IQ(200001:250000,1)=normrnd(100.5,14,ncondobs);
 IQ=round(IQ);
 
 % Create a vector of participant numbers
-Px=[1:250000]';
+Px=[1:nobs]';
 
 
 
 % Concatenate the data so far
-R2=[Px, Group, Age, IQ, R, RT];
-dlmwrite('loftus.csv',R2)
+
+ toto =    num2cell(Px);
+ toto1 = nomobs';
+ toto2 = num2cell([Group, Age, IQ, R, RT]);
+ 
+ R2=[toto toto1 toto2];
+ clear toto toto1 toto2
+
+k=0;
+k=k+1; colnames{k}='Participant';
+k=k+1; colnames{k}='Condition Name';
+k=k+1; colnames{k}='Condition Number';
+k=k+1; colnames{k}='Age';
+k=k+1; colnames{k}='IQ';
+k=k+1; colnames{k}='Estimated Speed (mph)';
+k=k+1; colnames{k}='Reaction Time (ms)';
+
+R2=[colnames;R2];
+
+% write line-by-line to csv file
+fid = fopen('loftus.csv','wt');
+for i=1:size(R2,1)
+    fprintf(fid, '%s,%d,%d\n', R2{i,:});
+end
+fclose(fid);
+
+ 
+
 
 
 
