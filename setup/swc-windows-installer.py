9b6a54a83b363cee784fcfc1fa493a14780e4df8
W. Trevor King <wking@tremily.us>
2013-10-20 22:35:00 -0700

    swc-windows-installer.py: De-emphasize Anaconda
    Bridge the gap between the original Anaconda plug and Ethan's simpler
    "Install Python" from 833d0ee (Add a Nano installer for Windows,
    2013-09-17).  As of Anaconda 1.7.0, IPython 1.0.0 and nose 1.3.0 are
    included by default [1].
    
    [1]: http://docs.continuum.io/anaconda/pkgs.html

--- setup/swc-windows-installer.py
+++ setup/swc-windows-installer.py
@@ -11,12 +11,14 @@ The script:
 
 To use:
 
-1. Install Anaconda CE Python distribution
+1. Install Python, IPython, and Nose.  An easy way to do this is with
+   the Anaconda CE Python distribution
    http://continuum.io/anacondace.html
 2. Install msysgit
    http://code.google.com/p/msysgit/downloads/list?q=full+installer+official+git
 3. Run swc_windows_installer.py
    You should be able to simply double click the file in Windows
+
 """
 
 try:  # Python 3
