4bacbe7ad17e3b49c5fdb8b14bf6556097ef0d4a
John Blischak <jdblischak@gmail.com>
2014-02-20 11:51:45 -0600

    Added R script to create html content for the full R intermediate bootcamp.

---
+++ intermediate/r/knit_intermediate_bootcamp.R
@@ -0,0 +1,34 @@
+#!/usr/bin/Rscript
+
+# Knits all the files for the SWC intermediate R bootcamp
+
+# Should be executed from within the directory: bc/intermediate/r
+
+library(markdown)
+library(knitr)
+
+cwd <- normalizePath(getwd())
+dirs <- strsplit(cwd, "/")[[1]]
+if (dirs[length(dirs)] != "r") {
+  stop("This script needs to be run from within bc/intermediate/r.")
+}
+
+# Knit Rmd files to html, remove intermediate md files
+files <- list.files(pattern = "Rmd$", recursive = TRUE, full.name = TRUE)
+files <- normalizePath(files)
+for (fname in files) {
+  setwd(dirname(fname))
+  knit2html(input = basename(fname), envir = new.env())
+  unlink(sub(".Rmd$", ".md", basename(fname)))
+}
+
+# Convert md files to html
+setwd(cwd)
+files <- list.files(pattern = "\\.md$", recursive = TRUE, full.name = TRUE)
+files <- normalizePath(files)
+for (fname in files) {
+  if (basename(fname) == "README.md") {
+    next
+  }
+  renderMarkdown(file = fname, output = sub("md$", "html", fname))
+}
