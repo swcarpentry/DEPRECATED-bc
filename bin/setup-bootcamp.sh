f66673e4465c52e46265ace5a95a4d0b4f27dd74
Raniere Silva <raniere@ime.unicamp.br>
2014-03-05 10:17:13 -0300

    Beta version of setup
    - Add request for password
    - Add password in requests
    - Remove echo commands

--- bin/setup-bootcamp.sh
+++ bin/setup-bootcamp.sh
@@ -18,7 +18,7 @@ function create_repo {
     # TODO: Add description
     url=https://api.github.com/user/repos
     ghpages=https://${OWNER}.github.io/${BOOTCAMPID}
-    echo curl -u "${OWNER}" -i \
+    curl -u "${OWNER}:${PASSWORD}" -i \
         -d "{\"name\":\"${BOOTCAMPID}\",\"homepage\":\"${ghpages}\"${REPO_DEFAULTS}}" \
         ${url}
 }
@@ -27,16 +27,19 @@ function set_team {
     for instructor in ${INSTRUCTORS};
     do
         url=https://api.github.com/repos/${OWNER}/${BOOTCAMPID}/collaborators/${instructor}
-        echo curl -u "${OWNER}" -i -X PUT \
+        curl -u "${OWNER}:${PASSWORD}" -i -X PUT \
             ${url}
     done
 }
 
-function clone {
-    echo git clone -b gh-pages -o bc https://github.com/swcarpentry/bc ../
-    echo cd ../${BOOTCAMPID}
-    echo git remote add origin ${BOOTCAMPURL}
-    echo git push -u origin gh-pages
+function clone_and_push {
+    git clone -b gh-pages -o bc \
+        https://github.com/swcarpentry/bc ../${BOOTCAMPID}
+    cd ../${BOOTCAMPID}
+    git remote add origin ${BOOTCAMPURL}
+    git push \
+        --repo ${BOOTCAMPURL/https:\/\//https:\/\/${OWNER}:${PASSWORD}@} \
+        gh-pages
 }
 
 function end_message {
@@ -49,18 +52,17 @@ function end_message {
     echo "2. Check the information at ../${BOOTCAMPID}/index.html"
     echo "3. Update the repository:"
     echo ""
-    echo "    $ git push gh-pages"
+    echo "    $ git push origin gh-pages"
 }
 
 function main {
     create_repo
     set_team
-    clone
+    clone_and_push
     end_message
 }
 
-# Call main function
-
+# Check the number of input arguments
 if test $# -lt 2
 then
     echo "You must provide at least the bootcamp id and"
@@ -68,10 +70,17 @@ then
     exit 1
 fi
 
+# Setup environments variables
 BOOTCAMPID=$1
 OWNER=$2
 BOOTCAMPURL=https://github.com/${OWNER}/${BOOTCAMPID}
 INSTRUCTORS=${@:3}
 REPO_DEFAULTS=,\"has_issues\":false,\"has_wiki\":false,\"has_downloads\":false
 
+# Ask for password
+echo "Enter the password for $OWNER:"
+# Read the password from user.
+read -s PASSWORD
+
+# Call main function
 main
