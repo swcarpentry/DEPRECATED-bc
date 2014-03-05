4c813b4855611ff7de3cdd35e77e789eba7f9f71
Raniere Silva <raniere@ime.unicamp.br>
2014-03-05 09:57:14 -0300

    Add unit tests to index.html validator
    - Add unit test to all check functions
    - Add unit test to file handler
    
    Changes in swc_index_validator.py
    
    - Remove some calls to `strip()`
    
      They aren't necessary any more because we use PyYAML.
    - Changes in `check_contry()`
    
      Use only one conditional
    - Change in `check_intructor()`
    
      Check if the list isn't empty.
    
    TODO:
    
    - Fix the unit test that fail.

---
+++ bin/test_swc_index_validator.py
@@ -0,0 +1,109 @@
+"""
+Test suite for ``validate_index.py``
+
+Bootcamp metadata is stored in yaml header and PyYaml
+strip all the strings.
+"""
+
+from io import StringIO
+from datetime import date
+import swc_index_validator
+
+def test_check_layout():
+    assert swc_index_validator.check_layout("bootcamp")
+
+def test_check_layout_fail():
+    assert not swc_index_validator.check_layout("lesson")
+
+def test_check_root():
+    assert swc_index_validator.check_root(".")
+
+def test_check_root_fail():
+    assert not swc_index_validator.check_root("setup")
+
+def test_check_contry():
+    assert swc_index_validator.check_country("Country")
+
+def test_check_contry_none():
+    assert not swc_index_validator.check_country(None)
+
+def test_check_contry_two_words():
+    assert not swc_index_validator.check_country("Some Country")
+
+def test_check_humandate():
+    assert swc_index_validator.check_humandate("Feb 18-20, 2525")
+
+def test_check_humandate_fail():
+    assert not swc_index_validator.check_humandate("February 18-20, 2525")
+
+def test_check_humandate_chars():
+    assert not swc_index_validator.check_humandate("XXX SomeDay, Year")
+
+def test_check_humantime():
+    assert swc_index_validator.check_humantime("09:00am")
+
+def test_check_humantime_fail():
+    assert not swc_index_validator.check_humantime("09:00")
+
+def test_check_humantime_only_am():
+    assert not swc_index_validator.check_humantime("am")
+
+def test_check_date():
+    assert swc_index_validator.check_date(date(2525, 2, 20))
+
+def test_check_date_fail():
+    assert not swc_index_validator.check_date("Feb 18-20, 2525")
+
+def test_check_latitude_longitude():
+    assert swc_index_validator.check_latitude_longitude("0.0,0.0")
+
+def test_check_latitude_longitude_chars():
+    assert not swc_index_validator.check_latitude_longitude("foo,bar")
+
+def test_check_registration_open():
+    assert swc_index_validator.check_registration("open")
+
+def test_check_registration_restricted():
+    assert swc_index_validator.check_registration("restricted")
+
+def test_check_registration_fail():
+    assert not swc_index_validator.check_registration("close")
+
+def test_check_instructor():
+    assert swc_index_validator.check_instructor(["John Doe", "Jane Doe"])
+
+def test_check_instructor_only_one():
+    assert swc_index_validator.check_instructor(["John Doe"])
+
+def test_check_instructor_empty():
+    assert not swc_index_validator.check_instructor([])
+
+def test_check_instructor_string():
+    assert not swc_index_validator.check_instructor("John Doe")
+
+def test_check_email():
+    assert swc_index_validator.check_email("user@box.com")
+
+def test_check_email_obfuscate():
+    assert not swc_index_validator.check_email("user AT box DOT com")
+
+def test_check_file():
+    header_sample = """---
+layout: bootcamp
+root: .
+venue: Euphoric State University
+address: 123 College Street, Euphoria
+country: United-States
+humandate: Feb 17-18, 2020
+humantime: 9:00 am - 4:30 pm
+startdate: 2020-06-17
+enddate: 2020-06-18
+latlng: 41.7901128,-87.6007318
+registration: restricted
+instructor: ["Grace Hopper", "Alan Turing"]
+contact: admin@software-carpentry.org
+---"""
+
+    file_ = StringIO(header_sample)
+
+    assert not swc_index_validator.check_file(file_)
