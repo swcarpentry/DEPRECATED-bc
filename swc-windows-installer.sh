47056b83143efb8ad816a8fb8b9ee095ba058bc0
W. Trevor King <wking@tremily.us>
2014-03-12 16:41:05 -0700

    swc-windows-installer.sh: Rename ~/.nanorc to ~/nano.rc
    The nano FAQ hints that this is the prefered path on MS Windows [1],
    and Ethan White confirmed it in testing [2].
    
    [1]: http://www.nano-editor.org/dist/v2.2/faq.html#3.9.1
    [2]: https://github.com/swcarpentry/bc/pull/357#issuecomment-37482899

--- swc-windows-installer.sh
+++ swc-windows-installer.sh
@@ -6,7 +6,7 @@
 #
 # The script:
 # * Installs nano and makes it accessible from msysGit
-# * Creates ~/.nanorc with links to syntax highlighting configs
+# * Creates ~/nano.rc with links to syntax highlighting configs
 # * Provides standard nosetests behavior (if Python and the nose
 #   module are installed) for msysGit
 #
@@ -91,11 +91,11 @@ install_nanorc()
 		'03233ae480689a008eb98feb1b599807' \
 		"${INSTALL_DIRECTORY}" \
 		--strip-components 1 &&
-	if [ ! -f ~/.nanorc ]
+	if [ ! -f ~/nano.rc ]
 	then
 		for RCPATH in "${INSTALL_DIRECTORY}"/doc/syntax/*.nanorc
 		do
-			echo "include ${RCPATH}" >> ~/.nanorc
+			echo "include ${RCPATH}" >> ~/nano.rc
 		done
 	fi
 }
